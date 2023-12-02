cad04e883f0af454d17e54ef01843925658c8829
Christian Roepke <christian.roepke@hotmail.de>
2023-12-02 15:34:48 +0100

    Imported "picozero" library into "Test_Button.py" and adapted program code accordingly

--- 02 Software/IMU_Sensorfusion/IMU_Sensorfusion_Christian_Roepke/Test_Button.py
+++ 02 Software/IMU_Sensorfusion/IMU_Sensorfusion_Christian_Roepke/Test_Button.py
@@ -1,30 +1,7 @@
-from machine import Pin, Timer
+from picozero import LED, Button
 
-# Initialization of GPIO21 as output
-led_onboard = Pin(21, Pin.OUT, value = 0)
-led_onboard.off()
+led = LED(21)
+button = Button(20)
 
-# Initialization of GPIO20 as input with external PULLUP resistor 
-btn = Pin(20, Pin.IN)
-
-# push-button function
-def on_pressed(timer):
-    led_onboard.toggle()
-    print('pressed')
-
-# debounce function
-def btn_debounce(pin):
-    # set timer (period in milliseconds)
-    Timer().init(mode = Timer.ONE_SHOT, period = 200, callback = on_pressed)
-
-# button triggering
-btn.irq(handler = btn_debounce, trigger = Pin.IRQ_FALLING)
-
-# button evaluation function
-while True:
-    # switch not pressed (logical "1")
-    if btn.value() == 1:
-        led_onboard.off()
-    # switch pressed (logical "0")
-    else:
-        led_onboard.on()
+button.when_pressed = led.on
+button.when_released = led.off
