36d984d3ab56522574d526483d0ca029d3c802d8
Christian Roepke <christian.roepke@hotmail.de>
2024-03-11 10:14:41 +0100

    - Version 1 überarbeitet

--- 02 Software/IMU_Sensorfusion/IMU_Sensorfusion_Christian_Roepke/MPU9250Data.py
+++ 02 Software/IMU_Sensorfusion/IMU_Sensorfusion_Christian_Roepke/MPU9250Data.py
@@ -1,12 +1,15 @@
 # author = 'Christian Roepke'
 # Program history: 
 #  06.03.24  V. 1.0  Start
+#  11.03.24  V. 1.1  
 # Functional description:
 # Package to define MPU9250 data class for time, accleration, gyro, magnetic and temperature data
 
 class MPU9250Data:
-    time: 0
-    acc:  1
-    gyro: 2
-    mag:  3
-    temp: 4
+
+    def sensData(self, time=0, acc=(0,0,0), gyro=(0,0,0), mag=(0,0,0), temp=0):
+        self.time = time
+        self.acc = acc
+        self.gyro = gyro
+        self.mag = mag
+        self.temp = temp
