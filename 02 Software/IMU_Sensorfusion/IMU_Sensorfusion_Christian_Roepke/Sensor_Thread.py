445f10d175479b247b89de329686cbdb14e42e14
Christian Roepke <christian.roepke@hotmail.de>
2024-03-12 13:57:05 +0100

    - Version 1.2 - globale Variablen (bis auf Timer) in eine extra Klasse ausgelagert - state Machine für Thread 1 implementiert und ausgelagert

--- 02 Software/IMU_Sensorfusion/IMU_Sensorfusion_Christian_Roepke/Sensor_Thread.py
+++ 02 Software/IMU_Sensorfusion/IMU_Sensorfusion_Christian_Roepke/Sensor_Thread.py
@@ -2,6 +2,7 @@
 # Program history: 
 #  04.03.24  V. 1.0  Start
 #  06.03.24  V. 1.1  Receive and read rotated data
+#  12.03.24  V. 1.2  Global variables replaced and integration state machine in Thread1
 # Functional description:
 #  Package for receive and read rotated data from MPU9250 sensor
 
@@ -11,9 +12,11 @@ import _thread
 import ustruct
 from machine import UART, Pin, I2C, Timer
 from mpu9250 import MPU9250
+from Flagclass import Flagclass
 from MPU9250Data import MPU9250Data
 from standardize_coordinate_system import rotate_axes
-  
+from Thread1_StateMachine import Thread1_StateMachine
+
 print('thread 1 start')
 
 # initialization UART0 and MPU9250 sensor
@@ -24,40 +27,31 @@ uart0_rxData = bytes()
 sensor = MPU9250(I2C(1, scl=Pin(7), sda=Pin(6), freq=400000))
 
 sensData = MPU9250Data
-Flag_SensorData = 0
-Flag_uart0_RX = 0
-Flag_run_Thread2 = 1
+Flags = Flagclass(0,0,1)
 
 # define thread function (including thread 2 with collect and read data): 
-def thread2():
+def thread2(Flags, uart0, uart0_rxData):
     print('thread 2 start')
-    
     def timT2_callback(timer):
         
-        global sensor
+        global sensor								# global variables cannot be transferred due to timer callback  
         global sensData
-        global Flag_SensorData
-        global Flag_run_Thread2
-        print('Timer callback')
+        global Flags
+        #print('Timer callback')
         
-        sensData.acc = sensor.acceleration			#functions from mpu9250.py file
+        sensData.acc = sensor.acceleration			# functions from mpu9250.py file
         sensData.gyro = sensor.gyro
         sensData.mag = sensor.magnetic
         sensData.temp = sensor.temperature
         
-        Flag_SensorData = 1;
+        Flags.Flag_SensorData = 1;
     
-    def uart0_RX_callback():
-        
-        global uart0
-        global uart0_rxData
-        global Flag_uart0_RX
-       
+    def uart0_RX_callback(uart0, uart0_rxData, Flags):
         if uart0.any() > 0:
             bf = uart0.read(1)
             if bf == b'\n':
                 uart0_rxData += bf
-                Flag_uart0_RX = 1
+                Flags.Flag_uart0_RX = 1
                 print('read newline')
             else:
                 uart0_rxData += bf
@@ -65,34 +59,29 @@ def thread2():
     timT2 = Timer()
     timT2.init(period=1000, callback=timT2_callback)
     
-    while Flag_run_Thread2==1:
-        uart0_RX_callback()
-        
-    timT2.deinit()
+    try:
+        while Flags.Flag_run_Thread2==1:
+            uart0_RX_callback(uart0, uart0_rxData, Flags)
+            
+    finally:
+        timT2.deinit()
     print('thread 2 end')     
 
-# call and pass function from second core
-_thread.start_new_thread(thread2, ())
+# call and pass function from second core (Thread 2)
+_thread.start_new_thread(thread2, (Flags, uart0, uart0_rxData,))
 
 # Thread 1 (receive data):
-led_onboard = Pin(25, Pin.OUT, value=1)
-
-while True:
-    #state machine
+try:
+    led_onboard = Pin(25, Pin.OUT, value=1)
+    state_machine = Thread1_StateMachine(Flags)
     
-    if Flag_SensorData == 1:
-        print('finish sensor data')
-        Flag_SensorData = 0								# set 0 for receive new data
-        sensData = rotate_axes(sensData)				# rotate coordinate system drehen
-        print('Acceleration rotated data:', sensData.acc)
-        print('Gyro rotated data:', sensData.gyro)
-        print('Magnetic rotated data:', sensData.mag)
-        print('Temperature data:', sensData.temp)
-    if Flag_uart0_RX == 1:
-        print('finish receive uart0 data')
-        Flag_uart0_RX = 0								# set 0 for receive new data
-        
-led_onboard.off()
-Flag_run_Thread2 = 0
-time.sleep(0.1)
-print('thread 1 end')
+    while True:
+        time.sleep(0.1)
+        #print('stateMachine call')
+        state_machine.StateMachine(sensData)
+
+finally:
+    Flags.Flag_run_Thread2 = 0
+    led_onboard.off()
+    time.sleep(0.1)
+    print('thread 1 end')
