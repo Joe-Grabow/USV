9722e1263f1846a42c69306fc0368de8e3c94e69
Christian Roepke <christian.roepke@hotmail.de>
2024-03-21 10:58:56 +0100

    - Erweiterung state Machine

--- 02 Software/IMU_Sensorfusion/IMU_Sensorfusion_Christian_Roepke/Python_Files_Roepke/Thread1_StateMachine.py
+++ 02 Software/IMU_Sensorfusion/IMU_Sensorfusion_Christian_Roepke/Python_Files_Roepke/Thread1_StateMachine.py
@@ -2,13 +2,14 @@
 # Program history: 
 #  12.03.24  V. 1.0  Start
 #  18.03.24  V. 1.1  define the state Machine as a class, including def __init__, extension of the state machine
+#  20.03.24  V. 1.2  extension of the state Machine
 # Functional description:
 # Package to define the state machine for IMU
 
 class Thread1_StateMachine:
     
     from standardize_coordinate_system import rotate_axes
-    from calibrating_routine_ak8963 import check_cal_data, calibration_routine_ak8963
+    from calibrating_routine_ak8963 import calibration_routine_ak8963
         
     def __init__(self,Flags):
         self.Flags = Flags
@@ -41,11 +42,15 @@ class Thread1_StateMachine:
             self.state = 1
         elif self.state == 1:
             # debug to state 8
-            self.state = 8
-#             if Thread1_StateMachine.check_cal_data('cal_data.json'):
-#                 self.state = 2
-#             else:
+            #self.state = 8
+            if self.Calibrate.check_cal_data():
+                self.Calibrate.load_cal_data()
+                print('read cal_data.json')
+                self.state = 2
+            else:
 #                 self.state = 3
+                # debug to state 8
+                self.state = 8
         elif self.state == 2:
             if self.Flags.Flag_SensorData == 1:
                 self.state = 4
@@ -53,9 +58,15 @@ class Thread1_StateMachine:
             if self.Flags.Flag_SensorData == 1:
                 self.state = 6
         elif self.state == 4:
-            print('state nicht vorhanden')
+            print('state 4')
+            self.sensData = sensData.copy()
+            self.sensData = Thread1_StateMachine.rotate_axes(self.sensData)
+            self.state = 5
         elif self.state == 5:
-            print('state nicht vorhanden')
+            print('state 5')
+            self.sensData.mag = self.Calibrate.valueCorrect(self.sensData.mag)
+            print(self.sensData.mag)
+            self.state = 2
         elif self.state == 6:
             self.sensData = sensData.copy()
             self.Flags.Flag_SensorData = 0							# set 0 for receive new data
@@ -68,7 +79,9 @@ class Thread1_StateMachine:
             #print('Temperature data:', self.sensData.temp)
             self.state = 3
         elif self.state == 8:
-            if self.Flags.Flag_SensorData == 1:
+            if self.Calibrate.bFinalCalibration == 1:
+                self.state = 11
+            elif self.Flags.Flag_SensorData == 1:
                 self.sensData = sensData.copy()
                 self.Flags.Flag_SensorData = 0
                 self.state = 9
@@ -79,3 +92,6 @@ class Thread1_StateMachine:
             self.Flags.Flag_SensorData = 0
             self.Calibrate.cal(self.sensData)
             self.state = 8
+        elif self.state == 11:
+            self.Calibrate.save_cal_data()
+            self.state = 2
